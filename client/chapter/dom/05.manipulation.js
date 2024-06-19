c4d319ab4eff3b7752e6658beb7c390185dcd8f1
JAM <99.1.some@gmail.com>
2024-06-19 10:36:24 +0900

    node manipulation

---
+++ client/chapter/dom/05.manipulation.js
@@ -0,0 +1,54 @@
+/* -------------------------- */
+/* DOM Manipulation           */
+/* -------------------------- */
+
+
+/* 노드 생성 메서드 --------------------------------------------------------- */
+
+// - document.createElement(tagName) – 태그 이름을 사용해 새로운 요소 생성
+// - document.createTextNode(value) – 새로운 텍스트 노드 생성
+// - element.cloneNode(deep) – 요소 복제. deep==true일 경우 모든 자손 요소도 복제
+
+const div = document.createElement('div');
+
+div.className = 'text-box'
+div.textContent='본문 시작'
+div.setAttribute('data-value', 1)
+
+console.log(div);
+
+document.body.append(div)
+
+
+/* 노드 삽입, 삭제 메서드 ---------------------------------------------------- */
+
+// - node.append(노드나 문자열) – node 끝에 노드를 삽입
+// - node.prepend(노드나 문자열) – node 맨 앞에 노드를 삽입
+// - node.before(노드나 문자열) – node 이전에 노드를 삽입
+// - node.after(노드나 문자열) – node 다음에 노드를 삽입
+// - node.replaceWith(노드나 문자열) – node를 대체
+// - node.remove() – node를 제거
+
+
+/* '오래된' 메서드 ----------------------------------------------------------- */
+
+// - parent.appendChild(node)
+// - parent.insertBefore(node, nextSibling)
+// - parent.removeChild(node)
+// - parent.replaceChild(newElement, node)
+
+
+/* 특정 위치에 삽입 --------------------------------------------------------- */
+
+// - insertAdjacentHTML
+
+const h1 = getNode('h1');
+document.body.insertAdjacentHTML("afterend", `<div class = "title"> like lion </div>`)
+let tag = ''
+
+function insertLast(tag, text){
+  tag = document.querySelector(tag);
+  tag.insertAdjacentHTML("beforeend", tag)
+}
+
+insertLast('body', tag);
