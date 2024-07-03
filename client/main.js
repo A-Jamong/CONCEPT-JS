aba47372f99c10712b0f649f854835aa72522929
JAM <99.1.some@gmail.com>
2024-07-03 13:46:22 +0900

    shadowdom 이벤트 걸기, this

--- client/main.js
+++ client/main.js
@@ -4,10 +4,27 @@ class UserCard extends HTMLElement{
     super();
     this.attachShadow({mode:'open'});
     this.shadowRoot.innerHTML = `
-    <div> nickName : kind-tiger </div>
-    <slot name="username">hello</slot>
-    <slot name="email">any@naver.com</slot>
+    <button type="button">btn</button>
     `
+
+    this.button = this.shadowRoot.querySelector('button');
+  }
+
+  // 생성되는 순간 실행되고 그럼 브라우저 돔에 연결이 확실히 되는 순간 호출되는 거라서 돔 이벤트 조작할 거면 여기에 모아주는 게 좋다.
+  connectedCallback(){
+    // console.log(this)
+    this.addEventListener('click',this.clickMe)
+    //* this.button.addEventListener('click',this.clickMe)
+    // 위에서 this를 출력하면 button 이 나온다. 근데 usercard가 나와야 한다.
+
+    // 화살표 함수 사용하기
+    this.button.addEventListener('click',()=>this.clickMe())
+    // 화살표함수에서 () 대신에 쓰게 해보기 bind
+    this.button.addEventListener('click',()=>this.clickMe.bind(this))
+  }
+
+  clickMe(){
+    console.log(this);
   }
 }
 
